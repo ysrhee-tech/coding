0fe0bfb451958a81d7775c419bf2c27c4c3743b4
ysrhee-tech <ysrhee@daum.net>
2020-05-19 11:44:00 +0900

    My.txt,test.py

--- Python/test.py
+++ Python/test.py
@@ -1,21 +1,18 @@
 # test.py
 
-for a in [1, 2, 3]:
-    print(a)
-
-print()
-
-i = 0
-while i < 3:
-    i = i + 1
-    print(i)
-
 a = "Life is too short, You need Python"
-print(a)
-print(a[:])
-print(a[0:4])
-print(a[19:])
-print(a[19:-7])
-print(a[19:-5])
+    #0123456789012345678901234567890123
+    #0         1         2         3
 
-print("%-10sjane" % "hi")
+b = a[0] + a[1] + a[2] + a[3]
+print(b)
+
+print(a[0:3])
+print(a[0:4])
+print(a[8:11])
+
+print(a[:4])
+print(a[19:])
+
+print(a[19:20])
+print(a[19:-7])
